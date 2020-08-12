5a9f7e478712eca3b3509eb3e1809e6f28f2d06f
Varadaraj <varadaraj.vv8@gmail.com>
2020-08-12 15:20:20 +0530

    Vacine Found

--- src/Disease/Corona.java
+++ src/Disease/Corona.java
@@ -4,6 +4,7 @@ public class Corona {
 
 	public static void main(String[] args) {
 		System.out.println("Corona disease came in the Year Dec 2019");
+		System.out.println("Vaccine Found in Year August 2020");
 	}
 
 }
